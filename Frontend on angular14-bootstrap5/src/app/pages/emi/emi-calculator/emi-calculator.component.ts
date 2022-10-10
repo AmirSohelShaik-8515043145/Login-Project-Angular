de23513f1b700b44f05a2879b92ff649dc7edb1d
AmirIncred <amir.shaik@incred.com>
2022-10-10 12:31:49 +0530

    emi-calculator updated ui added

--- Frontend on angular14-bootstrap5/src/app/pages/emi/emi-calculator/emi-calculator.component.ts
+++ Frontend on angular14-bootstrap5/src/app/pages/emi/emi-calculator/emi-calculator.component.ts
@@ -11,14 +11,27 @@ export class EmiCalculatorComponent implements OnInit {
   emiForm!: FormGroup;
   emi: number = 0;
   flag: boolean = false;
+  exposure: any;
+  array: any= [6, 12, 18, 24, 30, 36];
+  foreClosure:any;
+  test:any = 10
 
   constructor(private fb: FormBuilder, private auth: AuthService) {
 
     this.emiForm = this.fb.group({
-      'totalLoanAmount': ['', Validators.required],
-      'finalTenure': ['', Validators.compose([Validators.required, Validators.pattern(/(^\d{1}$)|(^\d{2}$)/)])],
-      'finalRoI': ['', Validators.required],
+      'topUp': ['', Validators.required],
+      'tenure': ['', Validators.required],
+      'roi': [18, Validators.required],
+      'foreClosure': ['', Validators.required],
+      'exposure': ['', Validators.required],
+      'pf': [2, Validators.required],
+      'dueDay':[],
+      'gst':[]
     })
+
+    if(this.emiForm.value.topUp){
+      this.foreClosure = this.emiForm.value.topUp+100000
+    }
   }
 
   ngOnInit(): void {
@@ -26,16 +39,21 @@ export class EmiCalculatorComponent implements OnInit {
 
   emiCalculator() {
     let data = this.emiForm.value;
+    console.log(data)
     this.flag = true
 
     let loanAmount = data.totalLoanAmount;
-    let interest = data.finalRoI/12/100;
+    let interest = data.finalRoI / 12 / 100;
     let tenure = data.finalTenure;
 
-    this.emi = loanAmount * interest * (Math.pow((1+interest),tenure)/(Math.pow((1+interest),tenure)-1))
-    if(this.emi%1 != 0){
+    this.emi = loanAmount * interest * (Math.pow((1 + interest), tenure) / (Math.pow((1 + interest), tenure) - 1))
+    if (this.emi % 1 != 0) {
       this.emi = Math.floor(this.emi)
     }
+
   }
+
+
+
 
 }
